cdd83c2382fc0c06f814278dacd78798d92dd3b8
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-08-16 00:41:57 -0500

    updating values

--- generators/audit/index.js
+++ generators/audit/index.js
@@ -12,6 +12,10 @@ const fsreader = require('fs');
 
 module.exports = class extends Generator {  
   writing() {
+      var yorc = this.destinationPath(".yo-rc.json");
+      var yorccontent = this.fs.read(yorc);
+      var packageFolder = JSON.parse(yorccontent)["generator-jhipster"].packageFolder;
+
       var scanDir = this.destinationPath("src/main/resources/config/liquibase/changelog");
       var templateFile = this.templatePath("audit-tables.xml");
       var templateContent = this.fs.read(templateFile);
@@ -23,6 +27,20 @@ module.exports = class extends Generator {
                 return newContent;
             }
         });
+      });
+
+      var excludedFiles = ['package-info.java','AbstractAuditingEntity.java','Authority.java','PersistentAuditEvent.java', 'User.java'];
+      scanDir = this.destinationPath("src/main/java/"+packageFolder+"/domain");
+      fsreader.readdirSync(scanDir).forEach(file => {
+        if(excludedFiles.indexOf(file)==-1){
+          this.fs.copy(this.destinationPath(scanDir+"/"+file), this.destinationPath(scanDir+"/"+file), {
+            process: function(content) {
+                var regEx = new RegExp('implements Serializable', 'g');
+                var newContent = content.toString().replace(regEx, 'extends AbstractAuditingEntity implements Serializable');
+                return newContent;
+            }
+        });
+        }
       });
   }
 };
