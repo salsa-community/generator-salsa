dbdc5c4f30c48a6e5acc17d8587b539c108d4192
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2021-06-07 17:25:01 -0500

    feat add validation for url login

--- generators/cvu/validator.js
+++ generators/cvu/validator.js
@@ -2,11 +2,15 @@ const GeneratorError = require('../util/GeneratorError');
 
 module.exports = class Validator {
     static validate(config) {
+        let generator = config.name.split('-')[1];
         if (!config.get('login-url')) {
-            throw new GeneratorError('Config Problem', 'The Login url is not defined, try to execute: ( yo conacyt:config )');
+            throw new GeneratorError(`Config Problem', 'The Login url is not defined, try to execute: ( yo ${generator}:config )`);
         }
         if (!config.get('reporte-ejecutivo-url')) {
-            throw new GeneratorError('Config Problem', 'The url for the cvu service is not defined, try to execute: ( yo conacyt:config )');
+            throw new GeneratorError(`Config Problem', 'The url for the cvu service is not defined, try to execute: ( yo ${generator}:config )`);
+        }
+        if (!config.get('reporte-completo-url')) {
+            throw new GeneratorError(`Config Problem', 'The url for the cvu service is not defined, try to execute: ( yo ${generator}:config )`);
         }
     }
 };
