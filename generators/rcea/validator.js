63fad7f016f89d3c88909d5ebe2a22ade4e16298
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2021-07-08 19:21:23 -0500

    update package

--- generators/rcea/validator.js
+++ generators/rcea/validator.js
@@ -7,10 +7,10 @@ const GeneratorError = require('../util/GeneratorError')
 module.exports = class Validator {
     static validate(config) {
         if (!config.get('login-url')) {
-            throw new GeneratorError('Config Problem', 'The Login url is not defined, try to execute: ( yo conacyt:config )');
+            throw new GeneratorError('Config Problem', 'The Login url is not defined, try to execute: ( yo crip:config )');
         }
         if (!config.get('reporte-ejecutivo-url')) {
-            throw new GeneratorError('Config Problem', 'The url for the cvu service is not defined, try to execute: ( yo conacyt:config )');
+            throw new GeneratorError('Config Problem', 'The url for the cvu service is not defined, try to execute: ( yo crip:config )');
         }
     }
 };
