3e914808befa86b115def6d7372ddcb0d9f48e99
daniel cortes pichardo <daniel.cortes.pichardo@gmail.com>
2022-06-19 13:05:34 -0500

    refactoring test data

--- generators/test/defaults.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Defaults {
-  static values(config) {
-    let context = {};
-    context.uiDescFile = config.get('ui-descripcion') ? config.get('ui-descripcion') : 'ui-descripcion.json';
-    return context;
-  }
-};
