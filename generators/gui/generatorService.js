bd58dc37951923a12b76ea10e76de8d7d69aa709
daniel cortes pichardo <daniel.cortes.pichardo@gmail.com>
2022-06-27 05:22:17 -0500

    refactoring doProcess for doGenerate

--- generators/gui/generatorService.js
+++ generators/gui/generatorService.js
@@ -4,27 +4,6 @@ const GeneratorHelper = require('./generatorHelper');
 const Visitor = require('./visitor/visitor');
 
 module.exports = class generatorService {
-  static doProcess(context) {
-    for (const seccionKey in context.secciones) {
-      let templateVariables = MapperHelper.defaultVariables(context.secciones, seccionKey);
-      let seccion = context.secciones[seccionKey];
-
-      for (const mayBeSubseccion in seccion) {
-        if (mayBeSubseccion != 'props') {
-          let subtemplateVariables = MapperHelper.defaultSubVariables(context.secciones, seccionKey, mayBeSubseccion);
-          let subseccion = context.secciones[seccionKey][mayBeSubseccion];
-
-          GeneratorHelper.writeEntitiesFiles(context, seccion, subseccion);
-          GeneratorHelper.writeUi(context, seccion, subtemplateVariables);
-          GeneratorHelper.writeComponentTs(context, seccion, subtemplateVariables);
-        }
-      }
-      GeneratorHelper.writeI18nFiles(context, seccion, templateVariables);
-    }
-    context.generator.config.set('secciones', context.seccionesOpt);
-    context.generator.config.save();
-  }
-
   static doGenerate(context) {
     Visitor.visit(context, context.model);
     context.generator.config.set('secciones', context.seccionesOpt);
